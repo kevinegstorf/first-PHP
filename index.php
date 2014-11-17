155e64ace88a66cb82b4286d0f9c3ffeacd3fa67
kevinegstorf <kevin.egstorf@gmail.com>
2014-11-17 17:10:54 +0100

    added email form and process

--- index.php
+++ index.php
@@ -1,29 +1,7 @@
-<html>
-<head>
-	<title><?php echo "Shirts 4 Mike"; ?></title>
-	<link rel="stylesheet" href="css/style.css" type="text/css">
-	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Oswald:400,700" type="text/css">
-	<link rel="shortcut icon" href="favicon.ico">
-</head>
-<body>
+<?php 
+$pageTitle = "Unique T-shirts designed by a frog";
 
-	<div class="header">
-
-		<div class="wrapper">
-
-			<h1 class="branding-title"><a href="./">Shirts 4 Mike</a></h1>
-
-			<ul class="nav">
-				<li class="shirts"><a href="#">Shirts</a></li>
-				<li class="contact"><a href="#">Contact</a></li>
-				<li class="cart"><a href="#">Shopping Cart</a></li>
-			</ul>
-
-		</div>
-
-	</div>
-
-	<div id="content">
+include('INC/header.php'); ?>
 
 		<div class="section banner">
 
@@ -73,22 +51,4 @@
 
 		</div>
 
-	</div>
-
-	<div class="footer">
-
-		<div class="wrapper">
-
-			<ul>		
-				<li><a href="http://twitter.com/treehouse">Twitter</a></li>
-				<li><a href="https://www.facebook.com/TeamTreehouse">Facebook</a></li>
-			</ul>
-
-			<p>&copy;<?php echo date('Y'); ?> Shirts 4 Mike</p>
-
-		</div>
-	
-	</div>
-
-</body>
-</html>
+	<?php include('inc/footer.php'); ?>
---
+++ shirts.php
@@ -0,0 +1,13 @@
+<?php 
+$pageTitle = "Mike's Full Catalog of Shirts";
+$section = "shirts";
+include('INC/header.php'); ?>
+
+  <div class="section page">
+
+  		<h1>Mike&rsquo;s Full Catalog Of Shirts</h1>
+
+
+  </div>
+
+  <?php include('inc/footer.php'); ?>
