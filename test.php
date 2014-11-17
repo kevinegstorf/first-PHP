091a023ce4d8c70fe1f0975f2ed68b072a2c0513
kevinegstorf <kevin.egstorf@gmail.com>
2014-11-17 11:32:13 +0100

    first PHP REPO

---
+++ test.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Shirts 4 Mike</title>
+</head>
+<body>
+	<h1>Shirts 4 Mike</h1>
+	<p>&copy;<?php echo date('Y'); ?> Shirts for Mike</p>
+
+</body>
+
+</html>
