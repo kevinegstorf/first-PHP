c19f9925953fdce667c0635ce3294b9b8804dfea
kevinegstorf <kevin.egstorf@gmail.com>
2014-11-18 22:24:18 +0100

    added array of shirt and altered contact

--- contact.php
+++ contact.php
@@ -1,3 +1,20 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+$name = $_POST["name"];
+$email = $_POST["email"];
+$message = $_POST["message"];
+$email_body = "";
+$email_body = $email_body . "Name: " . $name . "\n";
+$email_body = $email_body . "Email: " . $email . "\n";
+$email_body = $email_body . "Message: " . $message;
+
+// TODO: Send Email
+
+header("Location: contact.php?status=thanks"); 
+exit;
+
+}
+?>
 <?php 
 $pageTitle = "Contact Mike";
 $section = "contact";
@@ -8,43 +25,50 @@ include('INC/header.php'); ?>
   		<div class="wrapper">
 
   			<h1>Contact</h1>
+
+  			<?php if (isset($_GET["status"]) AND $_GET["status"] == "thanks") {
+  				?> <p>Thanks for the email! I&rsquo; ll be in touch shortly.</p> <?php 
+
+  			} else { ?>
+
 	
-			<p>I&rsquo;d love to hear from you! Complete the form te send me an email.</p>
+				<p>I&rsquo;d love to hear from you! Complete the form te send me an email.</p>
 
 
-			<form method="post" action="contact-process.php">
+				<form method="post" action="contact.php">
 
-				<table>
-					<tr>
-						<th>
-						<label for="name">Name</label> 
-						</th>
-						<td>
-						<input type="text" name="name" id="name">
-						</td>
-					</tr>
-					<tr>
-						<th>
-						<label for="email">Email</label> 
-						</th>
-						<td>
-						<input type="text" name="email" id="email">
-						</td>
-					</tr>
-
+					<table>
 						<tr>
-						<th>
-						<label for="message">Message</label> 
-						</th>
-						<td>
-						<textarea name="message" id="message"></textarea>
-						</td>
-					</tr>
-				</table>
-				<input type="submit" value="Send">
-			</form>
+							<th>
+							<label for="name">Name</label> 
+							</th>
+							<td>
+							<input type="text" name="name" id="name">
+							</td>
+						</tr>
+						<tr>
+							<th>
+							<label for="email">Email</label> 
+							</th>
+							<td>
+							<input type="text" name="email" id="email">
+							</td>
+						</tr>
 
-  		</div>
+							<tr>
+							<th>
+							<label for="message">Message</label> 
+							</th>
+							<td>
+							<textarea name="message" id="message"></textarea>
+							</td>
+						</tr>
+					</table>
+					<input type="submit" value="Send">
+				</form>
+
+		<?php } ?>
+	  	</div>
 
 
   </div>
